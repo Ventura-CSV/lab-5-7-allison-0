35a093e68e409ce690aa88545605e29d4f23ff46
allison-0 <allisonulrich00@gmail.com>
2024-07-01 20:26:42 -0700

    Edit

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 def splitlist(numbers):
     minval = numbers[0] # Assume that first element is the minimum
     minindex = 0
+    for i in range(1, len(numbers)):
+        if numbers[i] < minval:
+            # Update minval and minindex
+            minval = numbers[i]
+            minindex = i
 
 
 
