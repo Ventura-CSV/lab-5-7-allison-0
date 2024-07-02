95ec40cf815b574a77e89284e35f1418c8f73e9f
allison-0 <allisonulrich00@gmail.com>
2024-07-01 20:45:09 -0700

    Edit to use args

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
 
     numbers = [5, 4, 3, 2, 1]
 
-    first, others = splitlist(numbers)
+    first, others = splitlist(*numbers)
     print(first)  # Expected output     1
     print(others)  # Expected output     4 3 2 5
 
